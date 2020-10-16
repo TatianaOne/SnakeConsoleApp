b553d2b42ea1840e917d10d9a89955d774b5dec5
TatianaOne <tapusja2014@gmail.com>
2020-10-16 22:52:54 +0300

    Добавьте файлы проекта.

---
+++ SnakeConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using SnakeConsoleApp.UserServices;
+using System;
+
+namespace SnakeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UIService uIService = new UIService();
+            uIService.GetMenu();
+
+            while(true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                uIService.GetCommand(key.Key);
+            }
+        }
+    }
+}
